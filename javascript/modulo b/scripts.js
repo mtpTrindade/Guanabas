d0d66ab187dc7018d449fa444b68bbd136b78b22
Matheus Trindade <matheus.t.protasio@gmail.com>
2023-07-12 20:34:24 -0300

    Tratamento de Dados
    Exercícios com tratamento básico de dados, por enquanto tá meio lento mas já dá pra imaginar as funcionalidades e possibilidades do JS...

---
+++ javascript/modulo b/scripts.js
@@ -0,0 +1,5 @@
+// var nome = window.prompt('Olá! Qual o seu nome?')
+
+// document.write(`Olá ${nome}! Seu nome tem ${nome.length} letras... <br>`)
+
+// document.write(`Seu nome em letra maiúsculas é ${nome.toUpperCase()}...`)
