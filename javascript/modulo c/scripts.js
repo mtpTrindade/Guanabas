04c10ee235eed565930faa0a6f25c0de14e1d41c
Matheus Trindade <matheus.t.protasio@gmail.com>
2023-07-18 19:50:09 -0300

    Exercício Módulo D
    Montagem da pasta e dos arquivos para um exercício básico utilizando HTML, CSS e JS...

--- javascript/modulo c/scripts.js
+++ javascript/modulo c/scripts.js
@@ -21,6 +21,7 @@ Selecionando Elementos via DOM:
 */
 
 var poema = window.document.getElementById('poema')
+var clicked = false
 
 poema.addEventListener('click', clique)
 poema.addEventListener('mouseenter', hoverIn)
@@ -28,30 +29,31 @@ poema.addEventListener('mouseout', hoverOut)
 
 function clique(){
     var verso = window.document.getElementById('quintoVerso')
-    var clicked = false
-
+    
     if (clicked == false){
         verso.style.color = 'white'
         verso.style.textShadow = '1px 1px 2px black';
+
+        clicked = true
 
     }else{
         verso.style.color = 'transparent'
         verso.style.textShadow = 'none';
 
         clicked = false
-    }
-
-    clicked = true
+    }   
 }
 
 function hoverIn(){
     poema.style.backgroundColor = 'rgb(0, 200, 180)'
     poema.style.boxShadow = '2px 2px 4px black'
+    poema.style.transition = '0.3s'
 }
 
 function hoverOut(){
     poema.style.backgroundColor = 'rgb(30, 220, 180)'
     poema.style.boxShadow = 'none'
+    poema.style.transition = '0.3s'
 }
 
 // 
