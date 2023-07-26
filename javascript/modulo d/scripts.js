2f0688047df6449a486336842357f1335e99ec97
Matheus Trindade <matheus.t.protasio@gmail.com>
2023-07-26 11:03:15 -0300

    Exercício Módulo D - Parte V
    Finalmente terminei o design do exercício e e o conteúdo dele, agora falta configurar os botões de back e foward que eu tive a "brilhante" ideia de implementar...

--- javascript/modulo d/scripts.js
+++ javascript/modulo d/scripts.js
@@ -156,7 +156,7 @@ function Champion(){
 
                     divContainer.appendChild(paragrafo2)
                     paragrafo2.innerText = `Assim, o campeão em destaque daquele ano mais representa o player do que o campeão em si.\n
-                    Mas nesse caso foi o Tresh do MadLife, ele foi simplesmente o primeiro suporte da história a CARREGAR o time pro título!\n
+                    Mas nesse caso foi o Thresh do MadLife, ele foi o primeiro suporte da história a CARREGAR o time pro título!\n
                     O cara era simplesmente muito a frente do tempo dele, jogando o macro do jogo numa época que nem existia esse conceito.`
 
                     divContainer.appendChild(imageChampion)
@@ -214,100 +214,145 @@ function Champion(){
 
                 case '7':
                     divContainer.appendChild(paragrafo)
-                    paragrafo.innerText = ``
+                    paragrafo.innerText = `Season 7...\n
+                    No ano em que todo mundo esperava mais um título fácil da SKT a Samsung fez uma super fusão (literalmente) e veio pra surpreender na final, levando o título contra a própria SKT!\n
+                    Com destaque para o mid laner Crown, que fez um bootcamp no Brasil uns anos antes (com o nome de Shadow), e claramente a passagem dele aqui fez total diferença!\n
+                    Até porque jogar no server BR é tipo treinar naquela câmara de gravidade aumentada do Dragon Ball...`
 
                     divContainer.appendChild(imageTeam)
-                    imageTeam.src = 'images/.jpg'
+                    imageTeam.src = 'images/ssg.jpg'
 
                     divContainer.appendChild(paragrafo2)
-                    paragrafo2.innerText = ``
+                    paragrafo2.innerText = `Eu sei que o título da Samsung foi realmente algo surpreendente, mas o campeão em destaque na minha opinião foi o super Nocturne do Levi, da Gigabyte Marines...\n
+                    Os caras conseguiram fazer um Nocturne pegar level 6 antes de praticamente todo mundo do mapa, parecia que tava de hack!\n
+                    E obviamente que o Levi, tarado do jeito que era, acabou com o jogo, o cara era muito agressivo!`
 
                     divContainer.appendChild(imageChampion)
-                    imageChampion.src = 'images/.jpg'
+                    imageChampion.src = 'images/bc-noc.jpg'
                     break
 
                 case '8':
                     divContainer.appendChild(paragrafo)
-                    paragrafo.innerText = ``
+                    paragrafo.innerText = `Season 8...\n
+                    Depois de um domínio incontestável da Coréia nos últimos anos, a IG veio pra acabar com a palhaçada e colocar a China como uma potência pra rivalizar com a poderosa Coréia...\n
+                    Dito isso, foi realmente surpreendente o desempenho dos atiradores de todo o campeonato em geral, mas o JackeyLove realmente se sobressaiu e a Invictus Gaming levou o Wolrds naquele ano...`
 
                     divContainer.appendChild(imageTeam)
-                    imageTeam.src = 'images/.jpg'
+                    imageTeam.src = 'images/ig.jpg'
 
                     divContainer.appendChild(paragrafo2)
-                    paragrafo2.innerText = ``
+                    paragrafo2.innerText = `Ok, não me lembro de nada muito marcante e diferente naquele ano, então o campeão em destaque vai ser a Kai'sa, porque sim, ela teve um winrate absurdo, um milhão de highlights e a skin do time campeão...\n
+                    Pronto, é isso aí...`
 
                     divContainer.appendChild(imageChampion)
-                    imageChampion.src = 'images/.jpg'
+                    imageChampion.src = 'images/bc-kaisa.jpg'
                     break
 
                 case '9':
                     divContainer.appendChild(paragrafo)
-                    paragrafo.innerText = ``
+                    paragrafo.innerText = `Season 9! O ano da Fênix...\n
+                    Numa estranha conincidência, no mesmo ano em que a música do Mundial foi "Phoenix", quem ganhou o campeonato?\n
+                    Extamente! A Fun Plus PHOENIX!`
 
                     divContainer.appendChild(imageTeam)
-                    imageTeam.src = 'images/.jpg'
+                    imageTeam.src = 'images/fpx.jpg'
 
                     divContainer.appendChild(paragrafo2)
-                    paragrafo2.innerText = ``
+                    paragrafo2.innerText = `Enfim, o título continuou na China por mais um ano e o destaque vai pro Nautilus MID do DoinB!\n
+                    Uma das coisas mais escrotas e esquisitas do cenário competitivo na época...\n
+                    A FPX como um todo era um time exótico, mas ganharam né, então não tem o que discutir...`
 
                     divContainer.appendChild(imageChampion)
-                    imageChampion.src = 'images/.jpg'
+                    imageChampion.src = 'images/bc-nautilus.jpg'
                     break
 
                 case '10':
                     divContainer.appendChild(paragrafo)
-                    paragrafo.innerText = ``
+                    paragrafo.innerText = `Season 10!\n
+                    Mano, dez anos desse jogo de louco e um Wolrds que iria ser a altura, certo?\n
+                    Errado, foi um campeonato bem morno graças a campeã DAMWON Gaming, que tinha um estilo de jogo extremamente controlado...\n
+                    Efetivo? Sim, eles ganharam. Mas um porre de assistir...\n
+                    Ah, e sim, a Coréia voltou a ganhar uma né, isso virou uma disputa boa de Coréia vs China.`
 
                     divContainer.appendChild(imageTeam)
-                    imageTeam.src = 'images/.jpg'
+                    imageTeam.src = 'images/dwg.jpg'
 
                     divContainer.appendChild(paragrafo2)
-                    paragrafo2.innerText = ``
+                    paragrafo2.innerText = `E por falta de um destaque melhor eu vou escolher o Jhin como o campeão destaque daquele ano...\n
+                    Bom, eu gosto do Jhin, ele ganhou a skin da DWG e ele tinha muito poder dentro daquele jogo controladinho do meta, é isso...`
 
                     divContainer.appendChild(imageChampion)
-                    imageChampion.src = 'images/.jpg'
+                    imageChampion.src = 'images/bc-jhin.jpg'
                     break
 
                 case '11':
                     divContainer.appendChild(paragrafo)
-                    paragrafo.innerText = ``
+                    paragrafo.innerText = `Season 11, mais um título pra China...\n
+                    Dessa vez quem levou foi a EDG, graças aos 200+ anos de balanceamento da Rito Gomes.\n
+                    O meme nunca foi tão real!`
 
                     divContainer.appendChild(imageTeam)
-                    imageTeam.src = 'images/.jpg'
+                    imageTeam.src = 'images/edg.jpg'
 
                     divContainer.appendChild(paragrafo2)
-                    paragrafo2.innerText = ``
+                    paragrafo2.innerText = `Graças a Riot ficou bem difícil escolher o destaque daquele ano, tinha muito boneco quebrado!\n
+                    Mas ok, entre um emo, uma gata parasita e o rei do gado quem levou foi o emo, por muito pouco...`
 
                     divContainer.appendChild(imageChampion)
-                    imageChampion.src = 'images/.jpg'
+                    imageChampion.src = 'images/bc-aphelios.jpg'
+
+                    imageChampion.addEventListener("mouseenter", () => {
+                        imageChampion.src = 'images/bc-yuumi.jpg'
+                    })
+                    imageChampion.addEventListener("mouseout", () => {
+                        imageChampion.src = 'images/bc-aphelios.jpg'
+                    })
                     break
 
                 case '12':
                     divContainer.appendChild(paragrafo)
-                    paragrafo.innerText = ``
+                    paragrafo.innerText = `Ok, Season 12!\n
+                    Essa eu vou até me dedicar pra escrever, porque foi foda!\n
+                    De um lado tinhamos a T1 (antiga SKT), que tava amassando todo mundo e era a clara favorita ao título!\n
+                    Do outro a DRX, que niguém sabe nem como chegou no mundial, num mix de veteranos com novatos, conseguiram a vaga aos 47 do segundo tempo e conseguiram entrar no play-in...\n
+                    No lado da T1, tudo dentro dos conformes, um espanco atrás do outro até chegar na final.\n
+                    Já a DRX, meu deus, surpreendendo todo mundo, saiu da fase de grupos, jogou todo o playoff como underdog e chegou a tá 0-2 em duas MD5!
+                    Mesmo assim, chegaram na final contra a T1!\n
+                    Era um clássico Davi contra Golias!\n
+                    E quem ganhou? Obviamente que o Davi. Cara a DRX tinha um mental de ferro, uma resiliência inacreditável, começavam perdendo quase sempre e corriam atrás do resultado...\n
+                    Enfim, no maior miracle run da história desse game, a DRX, contra tudo e contra todos levou o campeonato daquele ano e hosnestamente, foi incrível!\n
+                    Todo mundo queria torcer pra eles, super carismáticos! O véio Deft jogando como sempre e o novato Zeka jogando como nunca!\n
+                    Foi um dos melhores campeonatos da história...`
 
                     divContainer.appendChild(imageTeam)
-                    imageTeam.src = 'images/.jpg'
+                    imageTeam.src = 'images/drx.jpg'
 
                     divContainer.appendChild(paragrafo2)
-                    paragrafo2.innerText = ``
+                    paragrafo2.innerText = `E por motivos de roteiro e protagonismo o campeão em destaque daquele ano vai ser a Caitlyn (que mal apareceu no competitivo) pelo simples fato de que é uma das preferidas do Deft e ele foi praticamente o protagonista dessa história digna de um documentário (que aliás existe, procura lá no lolsports).`
 
                     divContainer.appendChild(imageChampion)
-                    imageChampion.src = 'images/.jpg'
+                    imageChampion.src = 'images/bc-cait.jpg'
+
+                    imageChampion.addEventListener("mouseenter", () => {
+                        imageChampion.src = 'images/deft.jpg'
+                    })
+                    imageChampion.addEventListener("mouseout", () => {
+                        imageChampion.src = 'images/bc-cait.jpg'
+                    })
                     break
 
                 case '13':
                     divContainer.appendChild(paragrafo)
-                    paragrafo.innerText = ``
-
-                    divContainer.appendChild(imageTeam)
-                    imageTeam.src = 'images/.jpg'
-
-                    divContainer.appendChild(paragrafo2)
-                    paragrafo2.innerText = ``
+                    paragrafo.innerText = `E chegamos a Season 13...\n
+                    Nossa atual temporada, com um meta esquisito de atiradores AP, o véio tá machucado e talvez o Brasil ganhe o Mundial...\n
+                    Um grande talvez né, mas não é impossível, só muito difícil...\n
+                    Dito isso, obviamente ainda não temos o campeão, mas quando tivermos eu atualizo aqui...\n
+                    Ou não, mó preguiça, e isso aqui é só um exercício mesmo...\n
+                    Não, foco Matheus, tem que levar as coisas com seriedade!\n
+                    Enfim, também não temos campeão em destaque ainda, então fiquem com a Gwen Chibi do TFT porque eu tô jogando muito aquela merda e ela é fofíssima...`
 
                     divContainer.appendChild(imageChampion)
-                    imageChampion.src = 'images/.jpg'
+                    imageChampion.src = 'images/bc-gwen.jpg'
                     break
 
                 default:
@@ -329,8 +374,10 @@ function Champion(){
     seasonCheck = season
 }
 
-/*
-    O que falta?
-    - Fazer o conteúdo dinâmico realmente aparecer dentro da div;
-        
-*/
+function Foward(){
+    
+}
+
+function Back(){
+
+}
