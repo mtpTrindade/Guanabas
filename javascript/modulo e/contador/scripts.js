202f67056efce3fb2d6d5b2506b734225a758937
Matheus Trindade <matheus.t.protasio@gmail.com>
2023-07-31 20:16:44 -0300

    Exercício Módulo E - Final
    Aê caralho! Finalmente consegui resolver o problema (nunca mais erro a validação do for), por um lado eu tô aliviado que deu certo, por outro me sinto burro por passar três dias pra resolver algo tão simples....

--- javascript/modulo e/contador/scripts.js
+++ javascript/modulo e/contador/scripts.js
@@ -1,14 +1,14 @@
-
 //Definição das variáveis
-var start = document.querySelector('input#start')
-var end = document.querySelector('input#end')
-var step = document.querySelector('input#step')
 var main = document.querySelector('main')
 var resposta = document.createElement('div')
 resposta.id = 'resposta'
 var paragrafo = document.createElement('p')
 
+
 function calculo(){
+    var start = document.querySelector('input#start')
+    var end = document.querySelector('input#end')
+    var step = document.querySelector('input#step')
 
     if (start.value.length == 0 || end.value.length == 0 || step.value.length == 0){
         main.appendChild(resposta)
@@ -17,17 +17,26 @@ function calculo(){
     }else{
         main.appendChild(resposta)
         resposta.appendChild(paragrafo)
-        
-        for (var i = Number(start.value); Number(start.value) <= Number(end.value); i += Number(step.value)){
-            paragrafo.innerHTML += `${Number(start.value)} `
-        }
-        /*if (Number(start.value) <= Number(end.value)){
-            
+        console.log(start)
+
+        start = Number(start.value)
+        end = Number(end.value)
+        step = Number(step.value)
+
+        paragrafo.innerHTML = 'Contando: <br>'
+
+        if (start <= end){
+            for (var i = start; i <= end; i += step){
+                console.log(start)
+                paragrafo.innerHTML += ` ${i},`
+            }    
         } else {
-            for (var i = Number(start.value); Number(start.value) >= Number(end.value); i -= Number(step.value)){
-                paragrafo.innerHTML += `${Number(start.value)} `
+            for (var i = start; i >= end; i -= step){
+                paragrafo.innerHTML += ` ${i},`
             }
-        }*/
+        }
+
+        paragrafo.innerHTML += ' Fim.'
     }
       
 }
