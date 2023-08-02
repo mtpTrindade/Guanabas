dd6beffed3255b81fee38ff75030d61f86b1f768
Matheus Trindade <matheus.t.protasio@gmail.com>
2023-08-02 12:38:55 -0300

    Exercício II Módulo E
    Então, esse segundo exercício foi mais simples, apenas uma tabuada...

--- javascript/modulo e/tabuada/scripts.js
+++ javascript/modulo e/tabuada/scripts.js
@@ -1,1 +1,35 @@
+//Definição de variáveis de construção do HTML dinâmico
+var main = document.querySelector('main')
+var div = document.createElement('div')
+div.id = 'resposta'
+var select = document.createElement('select')
+select.id = 'tabuada'
+var paragrafo = document.createElement('p')
 
+function tabuada(){
+    var numero = document.querySelector('input#dataIn')
+    numero = numero.value
+
+    main.appendChild(div)
+
+    if (numero.length == 0){
+        div.appendChild(paragrafo)
+        paragrafo.innerHTML = `[ERRO] Insira um número para exibir a tabuada...`
+        div.removeChild(select)
+    } else{
+        div.appendChild(paragrafo)
+        paragrafo.innerHTML = `Tabuada de ${numero}:`
+
+        select.innerHTML = ''
+        div.appendChild(select)
+        select.size = '11'
+
+        for (var i = 0; i <= 10; i++){
+            var option = document.createElement('option')
+            select.appendChild(option)
+            option.value = `${i}`
+            option.innerHTML = `${numero} x ${i} = ${numero*i}`
+        }
+    }
+     
+}
