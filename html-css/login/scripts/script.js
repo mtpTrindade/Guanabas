97183e9a3958a5d4c9545c85f85a8faa42b31ccd
Matheus Trindade <matheus.t.protasio@gmail.com>
2023-08-09 10:07:56 -0300

    Tela de Login - Parte II
    Um pequeno commit só pra testar se a mudança de páginas tá funcionando normalmente no GitHub Pages...

---
+++ html-css/login/scripts/script.js
@@ -0,0 +1,23 @@
+
+function toCreateAccount(){
+    window.location.href = "https://mtptrindade.github.io/Guanabas/html-css/login/src/create-account.html";
+}
+
+function toLogin(){
+    window.location.href = "https://mtptrindade.github.io/Guanabas/html-css/login/src/login.html";   
+}
+
+function createAccount(){
+    window.alert(`Your account has been successfully created!\n
+    Kinda... 🙃`)
+}
+
+function login(){
+    window.alert(`Okay, you're logged in!\n  
+    That's all folks!\n 
+    Sorry, but I haven't done the rest of the code on this page yet... 😅`)
+}
+
+function forgotPassword(){
+    window.alert(`Well, if you don't remember your own password, I won't remember, right... 🤷‍♂️`)
+}
